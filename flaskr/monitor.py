a272e127ba60669ff502768e87f4a1f0bda6ad83
migo101 <migo101@gmx.fr>
2019-04-23 19:30:33 +0200

    Cleanup

--- flaskr/monitor.py
+++ flaskr/monitor.py
@@ -10,34 +10,32 @@ from flask import (
 bp = Blueprint('monitor', __name__, url_prefix='/')
 
 @bp.route('/dispute/<dispute_id>', methods=['GET'])
-def call(dispute_id):
+def dispute(dispute_id):
     node = 'https://mainnet.infura.io/v3/31c378f901bf46c08674e655e6640287'
 
     kleros = Kleros(node)
     case_Number = int(dispute_id)
 
     # Call kleros Smart-contract to get the total number of Jurors on current round
-
     dispute = kleros.dispute(case_Number)
-    jurors_drawn = dispute['draws_in_round']
+    j = dispute['draws_in_round']
 
-    # ("%s jurors drawn on last round" % jurors_drawn)
-    j = jurors_drawn
     ###main function call
     output = get_juror_votes(j, kleros, case_Number)
     return render_template('monitor/disputes.html', output=output)
+
+
 ### Main function, needs optimazation
 def get_juror_votes(j, kleros, case_Number):
-  output = {'dispute_id': case_Number, 'juror_count': j}
+  output = {
+    'dispute_id': case_Number,
+    'juror_count': j
+  }
 
-  if j == 3 or j == 5:
-    appeal = 0
-  elif j == 7 or j == 11:
-    appeal = 1
-  elif j == 15 or j == 23:
-    appeal = 2
-  else:
-    print("i haven't coded that part yet")
+  appeal = 0
+  if j == 7  or j == 11: appeal = 1
+  if j == 15 or j == 23: appeal = 2
+
 ### loop that retrieves all jurors votes and puts them in a list
   jurorVotes = []
   for i in range(j):
@@ -60,18 +58,18 @@ def get_juror_votes(j, kleros, case_Number):
 
   ###User-oriented, give information on majority reached or not
   if votesYes > j // 2 or votesNo > j // 2:
-    output['majority'] = ("Yes")
+    output['decided'] = "Yes"
   else:
-    output['majority'] = ("No")
+    output['decided'] = "No"
 
   ###simple way to define winner, probably will bug on some cases, need better logic
   jurorVotes.sort()
 
   index = (j // 2) + 1
   if jurorVotes[index] == 1:
-      output['outcome'] = ("Yes")
+      output['final'] = "Yes"
   elif jurorVotes[index] == 2:
-      output['outcome'] = ("No")
+      output['final'] = "No"
   else:
-    output['outcome'] = ("Undecided")
+    output['final'] = "Undecided"
   return output
