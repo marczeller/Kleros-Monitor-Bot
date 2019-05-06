2864da5c2be610ee7c59fa470e9d9fa77f81c942
Marc Zeller <pierarte@gmail.com>
2019-05-06 14:37:15 +0100

    added total_dispute event listener

--- jurors.py
+++ jurors.py
@@ -5,28 +5,31 @@ import os
 from kleros import Kleros, KlerosDispute, KlerosVote
 from collections import Counter, defaultdict
 import pprint
+import requests
 
+case_Number = 0
 node_url = os.environ["ETH_NODE_URL"]
-
+dispute = KlerosDispute(case_Number, node_url=node_url)
 pp = pprint.PrettyPrinter(indent=2)
-case_Number = 1
 juror_accounts = []
-while case_Number < 63:
+total_dispute = dispute.last_dispute_id()
+print (total_dispute)
+
+while case_Number <= total_dispute:
     dispute = KlerosDispute(case_Number, node_url=node_url)
     appeal = len(dispute.rounds) - 1
     jurors = dispute.rounds[-1]
 
     for i in range(jurors):
     	Votingdata = KlerosVote(case_Number, node_url=node_url, appeal = appeal, vote_id = i)
-    	dude_to_add = Votingdata.account
-    	juror_accounts.append(dude_to_add)        
+    	juror_accounts.append(Votingdata.account)        
     case_Number = case_Number + 1
 
 unique_jurors = dict(Counter(juror_accounts))
 new_unique_jurors = defaultdict(list)
 {new_unique_jurors[v].append(k) for k, v in unique_jurors.items()}
+vote_counter = sum(unique_jurors.values())
 
-dispute_number = case_Number - 1
 print("A total of %s unique jurors have been picked on Kleros" % len(unique_jurors))
-print("A total of %s disputes have been arbitraged on Kleros" % dispute_number)
-pp.pprint(new_unique_jurors)
+print("A total of %s disputes have been arbitraged on Kleros" % total_dispute)
+print("Jurors have casted a total of %s votes" % (vote_counter))
