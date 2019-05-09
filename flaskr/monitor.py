da2b583eefaed5ea53aac62599fd15c8f611c643
migo101 <migo101@gmx.fr>
2019-05-10 01:06:10 +0200

    Prepare data tree building

--- flaskr/monitor.py
+++ flaskr/monitor.py
@@ -3,7 +3,7 @@
 import sys
 sys.path.extend(('lib', 'db'))
 
-from kleros_db_schema import db, Dispute
+from kleros_db_schema import db, Dispute, Round, Vote
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -28,3 +28,9 @@ def dispute(id):
     dispute = Dispute.query.get(id)
 
     return render_template('monitor/dispute.html', dispute=dispute)
+
+
+# d = Dispute.query.get(17)
+# r = Round.query.filter(Round.dispute_id == d.id).all()
+# r2 = r[2]
+# v = Vote.query.filter(Vote.round_id == r2.id).all()
