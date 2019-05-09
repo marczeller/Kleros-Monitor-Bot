bb88c2a6f25c61d408d6653f95af1faeb4aa9de0
migo101 <migo101@gmx.fr>
2019-05-09 18:03:53 +0200

    Fix path

--- flaskr/monitor.py
+++ flaskr/monitor.py
@@ -10,7 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/kleros.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kleros.db'
 db = SQLAlchemy(app)
 
 from flask import (
