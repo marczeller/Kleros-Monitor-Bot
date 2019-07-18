7922ebe2493cfb480e9f80c31adbd487df9c2214
marczeller <pierarte@gmail.com>
2019-07-18 13:12:26 +0200

    add Kleros links, cleaned UI

--- bin/listshit.py
+++ bin/listshit.py
@@ -11,17 +11,7 @@ from kleros import Kleros, KlerosDispute, KlerosVote
 
 k = Kleros(os.environ["ETH_NODE_URL"])
 
-jurorlist = k.get_staking_jurors_list()
-x = len(jurorlist)
+jurorlist = k.get_juror_stakes()
 
-y = k.get_staking_jurors_len()
 
-z = k.get_staking_jurors_list()
-
-a = k.get_juror_stakeOf()
-
-f = k.get_len_stacking_dict()
-
-b = k.get_staking_average()
-
-print(b)
+print(jurorlist)
