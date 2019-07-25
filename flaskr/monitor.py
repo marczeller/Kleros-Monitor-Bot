2342a5fa876026c923b6a118b8d4643bd2864f7c
Marc Zeller <pierarte@gmail.com>
2019-07-25 09:20:53 +0200

    Revert "Full court"

--- flaskr/monitor.py
+++ flaskr/monitor.py
@@ -7,11 +7,8 @@ from kleros_db_schema import db, Dispute, Round, Vote, Kleroscan, Court
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-import statistics
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kleros.db'
-
 db = SQLAlchemy(app)
 
 from flask import (
@@ -26,40 +23,7 @@ def court(id):
     jurors = court.jurors()
     jurors_stats = court.juror_stats()
 
-    court_mapping = {
-        0: [2, 3, 4],
-        2: [3, 4,]
-    }
-    full_jurors = []
-    full_jurors_stats = {}
-    if id in court_mapping:
-        amounts = []
-        unique_jurors = {}
-        for j in jurors:
-            if j['address'] not in unique_jurors:
-                unique_jurors[j['address']] = {"staking_amount": j['staking_amount']}
-            else:
-                unique_jurors[j['address']]["staking_amount"] += j['staking_amount']
-            amounts.append(j['staking_amount'])
-
-        courts = Court.query.filter(Court.id.in_(court_mapping[id]))
-        for court in courts:
-            court_jurors = court.jurors()
-            for cj in court_jurors:
-                if cj['address'] not in unique_jurors:
-                    unique_jurors[cj['address']] = {"staking_amount": cj['staking_amount']}
-                else:
-                    unique_jurors[cj['address']]["staking_amount"] += cj['staking_amount']
-                amounts.append(cj['staking_amount'])
-
-        full_jurors_stats['length'] = len(amounts)
-        full_jurors_stats['mean'] = statistics.mean(amounts)
-        full_jurors_stats['median'] = statistics.median(amounts)
-        full_jurors = [{'address': address, 'staking_amount': juror['staking_amount']} for address, juror in unique_jurors.items()]
-        full_jurors = sorted(full_jurors, key=lambda j: j['staking_amount'], reverse=True)
-
-    return render_template('monitor/court.html', court=court, disputes=disputes, jurors=jurors, last_updated=kleroscan.value, 
-        jurors_stats=jurors_stats, full_jurors=full_jurors, full_jurors_stats=full_jurors_stats)
+    return render_template('monitor/court.html', court=court, disputes=disputes, jurors=jurors, last_updated=kleroscan.value, jurors_stats=jurors_stats)
 
 @app.route('/', methods=['GET'])
 @app.route('/disputes', methods=['GET'])
