c1de40f1473ea442906b0a59461ad56d7d78d43b
marczeller <pierarte@gmail.com>
2019-08-14 13:29:32 +0200

    ulgy temp ETH bot

--- bin/get_json.py
+++ bin/get_json.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
 #TO DO convert me into a lib class and connect that into fill_db.py
-
+import getopt
+import sys
+from datetime import datetime
+from time import gmtime, strftime
+sys.path.extend(('lib', 'db'))
+import os
 import requests
+from makerdao_medianizer import MakerDAO_Medianizer
+makerdao_medianizer = MakerDAO_Medianizer(os.environ["ETH_NODE_URL"])
+eth_price = makerdao_medianizer.eth_price()
 
 address = '0x916deaB80DFbc7030277047cD18B233B3CE5b4Ab'
 url = "https://api.etherscan.io/api?module=account&action=txlist&address=0x916deaB80DFbc7030277047cD18B233B3CE5b4Ab&startblock=7303699&endblock=99999999&sort=asc&apikey=GAN3HIU7QHKSP27MGXJPAAXUXEKVCU8C4X"
@@ -27,4 +35,9 @@ total = 0
 for i in range(0,val_len):
 	total += value_dump[i]
 
-print(total / 10 ** 18)
+total_ETH = total / 10 ** 18
+total_USD = total_ETH * eth_price
+
+print("total_ETH deposited : %3.f ETH" % total_ETH)
+print("eth_price is %2.f USD" % eth_price)
+print("total_USD deposited %3.f USD" % total_USD)
