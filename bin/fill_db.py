898c874047df75d08477c1e8b4b8d42c1fd509b6
marczeller <pierarte@gmail.com>
2019-08-15 13:37:40 +0200

    fix typo

--- bin/fill_db.py
+++ bin/fill_db.py
@@ -27,10 +27,6 @@ except getopt.GetoptError as err:
 def rebuild_db():
     db.drop_all()
     db.create_all()
-<<<<<<< HEAD
-
-=======
->>>>>>> 6a8aa318d1dd2a28eed769f6c9513aa4b14eb66f
     db.session.add(Court( id = 0, name = "General Court", address = "0x0d67440946949fe293b45c52efd8a9b3d51e2522"))
     db.session.add(Court( id = 1, name = "Court 1", address = ""))
     db.session.add(Court( id = 2, name = "TCR Court", address = "0xebcf3bca271b26ae4b162ba560e243055af0e679"))
