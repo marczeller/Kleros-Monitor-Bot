78cf6f0387f8f14307a6adeaff88d27fae1280bc
marczeller <pierarte@gmail.com>
2019-08-16 11:01:54 +0200

    uptaded test file

--- bin/get_json.py
+++ bin/get_json.py
@@ -3,7 +3,7 @@
 import sys
 sys.path.extend(('lib', 'db'))
 
-from kleros import db, Dispute, Round, Vote, Config, Court, JurorStake, Deposit
+from kleros import db, Dispute, Round, Vote, Config, Court, JurorStake, Deposit, Juror
 
 eth_price = float(Config.get('eth_price'))
 round_price = round(eth_price, 2)
@@ -12,7 +12,24 @@ round_eth = round(total_ETH, 2)
 total_USD = total_ETH * eth_price
 round_total = round(total_USD, 2)
 
-print("total_ETH deposited : %.2f ETH" % round_eth)
-print("eth_price is %.2f USD" % round_price)
-print(type(round_price))
-print("total_USD deposited %.5f USD" % round_total)
+
+#print(j)
+#print(len(j))
+total_jurors = 0
+total_votes = 0
+for court in range(5):
+	c = Court.query.get(court)
+	j = c.jurors
+	total_jurors += len(j)
+	for vote in range(len(j)):
+		votes = j[vote]['votes']
+		total_votes += votes
+#print(len(j))
+#print(total_jurors)
+#print("total_ETH deposited : %.2f ETH" % round_eth)
+#print("eth_price is %.2f USD" % round_price)
+#print("total_USD deposited %.5f USD" % round_total)
+
+a = Juror.list()
+print(a)
+print(len(a))
